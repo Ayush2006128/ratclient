10224a08028d0ebb3c60187daabbd73c3ec074fa
Ayush2006128 <74146837+Ayush2006128@users.noreply.github.com>
2023-10-13 16:53:20 +0530

    ControlerActivity added

---
+++ app/src/main/java/tech/ayushmuley1907/ratclient/ControlerActivity.kt
@@ -0,0 +1,11 @@
+package tech.ayushmuley1907.ratclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ControlerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_controler)
+    }
+}
